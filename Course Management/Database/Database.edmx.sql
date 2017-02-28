b571a7bd5b765a19f69b7244c9b16e9ce59b9d59
Shayan Khaksar <Sh.khaksar@yahoo.com>
2017-02-28 16:08:44 +0330

    Crate Entity and Databses

---
+++ Course Management/Database/Database.edmx.sql
@@ -0,0 +1,138 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 02/28/2017 15:57:22
+-- Generated from EDMX file: C:\dev\C# Projects\Course-Management-System\Course Management\Database.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [course_management];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Courses'
+CREATE TABLE [dbo].[Courses] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [name] nvarchar(max)  NOT NULL,
+    [capacity] nvarchar(max)  NOT NULL,
+    [exam_date] nvarchar(max)  NOT NULL,
+    [exam_time] nvarchar(max)  NOT NULL,
+    [time1] nvarchar(max)  NOT NULL,
+    [time2] nvarchar(max)  NOT NULL,
+    [TeacherId] int  NOT NULL,
+    [CareerId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Teachers'
+CREATE TABLE [dbo].[Teachers] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Students'
+CREATE TABLE [dbo].[Students] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'StudentCourse'
+CREATE TABLE [dbo].[StudentCourse] (
+    [Students_Id] int  NOT NULL,
+    [Courses_Id] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Courses'
+ALTER TABLE [dbo].[Courses]
+ADD CONSTRAINT [PK_Courses]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Teachers'
+ALTER TABLE [dbo].[Teachers]
+ADD CONSTRAINT [PK_Teachers]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Students'
+ALTER TABLE [dbo].[Students]
+ADD CONSTRAINT [PK_Students]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Students_Id], [Courses_Id] in table 'StudentCourse'
+ALTER TABLE [dbo].[StudentCourse]
+ADD CONSTRAINT [PK_StudentCourse]
+    PRIMARY KEY CLUSTERED ([Students_Id], [Courses_Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [TeacherId] in table 'Courses'
+ALTER TABLE [dbo].[Courses]
+ADD CONSTRAINT [FK_TeacherCourse]
+    FOREIGN KEY ([TeacherId])
+    REFERENCES [dbo].[Teachers]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_TeacherCourse'
+CREATE INDEX [IX_FK_TeacherCourse]
+ON [dbo].[Courses]
+    ([TeacherId]);
+GO
+
+-- Creating foreign key on [Students_Id] in table 'StudentCourse'
+ALTER TABLE [dbo].[StudentCourse]
+ADD CONSTRAINT [FK_StudentCourse_Student]
+    FOREIGN KEY ([Students_Id])
+    REFERENCES [dbo].[Students]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Courses_Id] in table 'StudentCourse'
+ALTER TABLE [dbo].[StudentCourse]
+ADD CONSTRAINT [FK_StudentCourse_Course]
+    FOREIGN KEY ([Courses_Id])
+    REFERENCES [dbo].[Courses]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_StudentCourse_Course'
+CREATE INDEX [IX_FK_StudentCourse_Course]
+ON [dbo].[StudentCourse]
+    ([Courses_Id]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
